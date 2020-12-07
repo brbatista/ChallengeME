6789904b6eeadf91f0a857b0ef53d06fca5c9e39
Beatriz <brbbeatriz@gmail.com>
2020-12-07 09:51:22 -0300

    Criado gerenciador de mensagens

---
+++ Negocio/Contratos/IGerenciadorMensagens.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negocio.Contratos
+{
+    public interface IGerenciadorMensagens
+    {
+        void AdicionarMensagem(string mensagem);
+        List<string> ListarMensagens();
+        bool PossuiMensagem();
+
+    }
+}
---
+++ Negocio/Mensagens/GerenciadorMensagens.cs
@@ -0,0 +1,33 @@
+﻿using Negocio.Contratos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Negocio.Mensagens
+{
+    public class GerenciadorMensagens : IGerenciadorMensagens
+    {
+        public List<string> Mensagens;
+
+        public GerenciadorMensagens()
+        {
+            Mensagens = new List<string>();
+        }
+
+        public void AdicionarMensagem(string mensagem)
+        {
+            Mensagens.Add(mensagem);
+        }
+
+        public List<string> ListarMensagens()
+        {
+            return Mensagens;
+        }
+
+        public bool PossuiMensagem()
+        {
+            return Mensagens.Any();
+        }
+    }
+}
